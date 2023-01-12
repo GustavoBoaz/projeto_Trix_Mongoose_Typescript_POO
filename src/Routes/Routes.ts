dcf05c3b615f0fd58a49fe61462926bccb66e921
GustavoBoaz <gustavo.boaz@hotmail.com>
2023-01-12 11:45:03 -0300

    @GustavoBoaz [Aula30.C2 - Construindo as camadas Service e Controller]

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import TransferController from '../Controllers/TransferController';
+import KeyController from '../Controllers/KeyController';
 
 const routes = Router();
 
@@ -21,6 +22,11 @@ routes.get(
 routes.get(
   '/transfer/:key',
   (req, res, next) => new TransferController(req, res, next).getByKey(),
+);
+
+routes.post(
+  '/key/register',
+  (req, res, next) => new KeyController(req, res, next).create(),
 );
 
 export default routes;
