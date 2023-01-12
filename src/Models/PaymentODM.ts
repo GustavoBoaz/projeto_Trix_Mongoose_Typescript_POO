3c97de26c358e9b41fda57b4a8dd13fdd1e62e67
GustavoBoaz <gustavo.boaz@hotmail.com>
2023-01-12 12:43:41 -0300

    @GustavoBoaz [Aula30.A2 - Refactoring na classe PaymentODM]

--- src/Models/PaymentODM.ts
+++ src/Models/PaymentODM.ts
@@ -7,35 +7,18 @@ import {
   UpdateQuery,
 } from 'mongoose';
 import IPayment from '../Interfaces/IPayment';
+import AbstractODM from './AbstractODM';
 
-class PaymentODM {
-  private schema: Schema; // Atributo para o "molde"
-  private model: Model<IPayment>; // Atributo para criar coleção e fornecer acesso ao banco
-
+class PaymentODM extends AbstractODM<IPayment> {
   constructor() {
-    this.schema = new Schema<IPayment>({
+    const schema = new Schema<IPayment>({
       payByPerson: { type: String, required: true },
       payToPerson: { type: String, required: true },
       amount: { type: Number, required: true },
       key: { type: String, required: true },
       status: { type: Number },
     });
-    this.model = models.Payment || model('Payment', this.schema); // Antes de criar o Schema, verificar se o schema já existe. Caso não exista, o schema será criado. 
-  }
-
-  public async create(payment: IPayment): Promise<IPayment> {
-    return this.model.create({ ...payment });
-  }
-
-  public async update(id: string, obj: Partial<IPayment>): 
-  Promise<IPayment | null> {
-    if (!isValidObjectId(id)) throw Error('Invalid Mongo id');
-    
-    return this.model.findByIdAndUpdate(
-      { _id: id },
-      { ...obj } as UpdateQuery<IPayment>,
-      { new: true },
-    );    
+    super(schema, 'Payment');
   }
 
   public async find(): Promise<IPayment[]> {
